10ce00d1f67d5fdee9231ac00e23bc9272d2bf7b
bitbyte08 <eight2823@naver.com>
2025-03-20 13:14:54 +0900

    Feat :: 앞면 뒷면 구현

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,26 +4,48 @@ import {useState} from "react";
 const Container = styled.article<{isFlip : boolean}>`
   width: 6.25rem;
   height: 9.375rem;
-  border-radius: 0.3125rem;
-  background-color: #535bf2;
-  transition: transform ease-in-out 100ms;
+  margin: 0;
+  padding: 0;
+  transition: transform ease-in-out 300ms;
+  display: inline-grid;
+  transform-style: preserve-3d;
   &:hover{
     transform: scale(1.2)
     ${({isFlip})=>
         (isFlip ?  "perspective(800px) rotateY(0deg)":  "rotateY(180deg);")}
   };
-  transform :${({isFlip})=>
-      (isFlip ?  "perspective(800px) rotateY(0deg)":  "rotateY(180deg);")}
+  transform: ${({isFlip})=>
+        (isFlip ?  "perspective(800px) rotateY(0deg)":  "rotateY(180deg);")}
+`;
+
+const Content = styled.section`
+  grid-area: 1/1/1/1;
+  backface-visibility: hidden;
+  border-radius: 0.3125rem;
+  height: 100%;
+  width: 100%;
+`;
+
+const FrontContent = styled(Content)<{isFlip: boolean}>`
+    background-color: blue;
+`;
+
+const BackContent = styled(Content)<{isFlip: boolean}>`
+    transform: rotateY(180deg);
+    background-color: white;
 `;
 
 const Card = () => {
   const [isFlip, setIsFlip] = useState(false);
   return (
-      <>
-        <Container isFlip={isFlip} onClick={()=>setIsFlip(!isFlip)}>
+      <Container isFlip={isFlip} onClick={()=>setIsFlip(!isFlip)}>
+        <FrontContent isFlip={isFlip}>
 
-        </Container>
-      </>
+        </FrontContent>
+        <BackContent isFlip={isFlip}>
+
+        </BackContent>
+      </Container>
   )
 }
 export default Card
